d07cc94465b32d154eb840dd754b1c75d89b54f5
minierparedes <3thancr0wn@gmail.com>
2024-05-06 23:09:48 +0900

    test(sql): add UpdateWebsite tests

--- db/sqlc/website_test.go
+++ db/sqlc/website_test.go
@@ -40,19 +40,48 @@ func TestCreateWebsite(t *testing.T) {
 
 func TestGetWebsite(t *testing.T) {
 	// Arrange
-	user, err := testQueries.GetUsers(context.Background(), 1)
+	user, err := testQueries.GetUsers(context.Background(), 9)
 	require.NoError(t, err)
 	require.NotEmpty(t, user)
 
-	template, err := testQueries.GetTemplate(context.Background(), 1)
+	template, err := testQueries.GetTemplate(context.Background(), 9)
 	require.NoError(t, err)
 	require.NotEmpty(t, template)
 	// Act
-	website, err := testQueries.GetWebsite(context.Background(), 1)
+	website, err := testQueries.GetWebsite(context.Background(), 9)
 	require.NoError(t, err)
 	require.NotEmpty(t, website)
 
 	// Assert
 	require.Equal(t, user.ID, website.OwnerID)
 	require.Equal(t, template.ID, website.SelectedTemplate)
+}
+
+func TestUpdateWebsite(t *testing.T) {
+	// Arrange
+	user, err := testQueries.GetUsers(context.Background(), 9)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	template, err := testQueries.GetTemplate(context.Background(), 9)
+	require.NoError(t, err)
+	require.NotEmpty(t, template)
+
+	args := UpdateWebsiteParams{
+		ID:               9,
+		Name:             "Mystery on Monster Island",
+		Domain:           "berkeley.edu",
+		OwnerID:          9,
+		SelectedTemplate: 9,
+	}
+	// Act
+	website, err := testQueries.UpdateWebsite(context.Background(), args)
+	// Assert
+	require.NoError(t, err)
+	require.NotEmpty(t, website)
+	require.Equal(t, args.ID, website.ID)
+	require.Equal(t, args.Name, website.Name)
+	require.Equal(t, args.Domain, website.Domain)
+	require.Equal(t, args.OwnerID, website.OwnerID)
+	require.Equal(t, args.SelectedTemplate, website.SelectedTemplate)
 }
