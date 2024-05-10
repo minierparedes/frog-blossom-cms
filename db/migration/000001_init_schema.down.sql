311dceb1f1784afe37644858a5228828377e13f4
minierparedes <3thancr0wn@gmail.com>
2024-05-10 12:16:15 +0900

    chore(sql): update schema to v4

--- db/migration/000001_init_schema.down.sql
+++ db/migration/000001_init_schema.down.sql
@@ -1,12 +1,10 @@
 -- Drop foreign key constraint before dropping table
-ALTER TABLE website DROP CONSTRAINT website_owner_id_fkey;
+-- ALTER TABLE website DROP CONSTRAINT website_owner_id_fkey;
 
 -- Drop tables that do not have dependencies
 DROP TABLE IF EXISTS meta;
-DROP TABLE IF EXISTS page_components;
 DROP TABLE IF EXISTS pages;
 DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS websites;
 
 -- Drop table after dropping the foreign key constraint
 DROP TABLE IF EXISTS users;
