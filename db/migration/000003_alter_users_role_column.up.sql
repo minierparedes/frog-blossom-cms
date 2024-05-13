f2758bc9cf84829ef7eb962931ac1b2779e26337
minierparedes <3thancr0wn@gmail.com>
2024-05-13 11:58:31 +0900

    chore(sql): alter role's type var -> enum

---
+++ db/migration/000003_alter_users_role_column.up.sql
@@ -0,0 +1,11 @@
+-- Alter the users table to change the role column type
+DO $$
+BEGIN
+    IF EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
+        ALTER TABLE users
+            DROP COLUMN IF EXISTS role;
+
+        ALTER TABLE users
+            ADD COLUMN IF NOT EXISTS role access NOT NULL;
+    END IF;
+END $$;
