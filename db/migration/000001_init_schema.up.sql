76aa5dc5375e00768ced51886028bd0371fb7569
minierparedes <3thancr0wn@gmail.com>
2024-05-13 09:41:01 +0900

    chore(sql): schema update v4 -> v5

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -3,11 +3,11 @@ CREATE TABLE "users" (
   "username" varchar(255) NOT NULL,
   "email" varchar(255) NOT NULL,
   "password" varchar(255) NOT NULL,
-  "role" varchar(255) DEFAULT 'user',
+  "role" varchar(255) NOT NULL,
   "first_name" varchar(255) NOT NULL,
   "last_name" varchar(255) NOT NULL,
   "user_url" text,
-  "bio" text,
+  "description" text,
   "created_at" timestamptz NOT NULL DEFAULT (now()),
   "updated_at" timestamptz NOT NULL
 );
@@ -15,7 +15,8 @@ CREATE TABLE "users" (
 CREATE TABLE "pages" (
   "id" bigserial UNIQUE PRIMARY KEY NOT NULL,
   "domain" varchar(255) NOT NULL,
-  "page_author" bigint NOT NULL,
+  "author_id" bigint NOT NULL,
+  "page_author" varchar(255) NOT NULL,
   "title" varchar(255) NOT NULL,
   "url" varchar(255) NOT NULL,
   "menu_order" bigint NOT NULL,
@@ -32,14 +33,17 @@ CREATE TABLE "posts" (
   "id" bigserial UNIQUE PRIMARY KEY NOT NULL,
   "title" varchar(255) NOT NULL,
   "content" text NOT NULL,
-  "author" varchar(255) NOT NULL,
+  "author_id" bigint NOT NULL,
+  "url" varchar(255) NOT NULL,
   "created_at" timestamp NOT NULL DEFAULT (now()),
   "updated_at" timestamp NOT NULL,
   "status" varchar(255) NOT NULL,
   "published_at" timestamp NOT NULL,
   "edited_at" timestamp NOT NULL,
   "post_author" bigint NOT NULL,
-  "post_mime_type" varchar(255) NOT NULL
+  "post_mime_type" varchar(255) NOT NULL,
+  "published_by" varchar(255) NOT NULL,
+  "updated_by" varchar(255) NOT NULL
 );
 
 CREATE TABLE "meta" (
@@ -49,8 +53,8 @@ CREATE TABLE "meta" (
   "meta_title" varchar(255),
   "meta_description" text,
   "meta_robots" varchar(255),
-  "meta_viewport" varchar(255),
-  "meta_charset" varchar(255),
+  "meta_og_image" varchar(255),
+  "locale" varchar(10),
   "page_amount" bigint NOT NULL,
   "site_language" varchar(255),
   "meta_key" varchar(255) NOT NULL,
@@ -58,31 +62,26 @@ CREATE TABLE "meta" (
 );
 
 CREATE INDEX ON "users" ("username");
-
 CREATE INDEX ON "users" ("first_name");
-
 CREATE INDEX ON "users" ("last_name");
-
 CREATE INDEX ON "users" ("created_at", "updated_at");
 
 CREATE INDEX ON "pages" ("page_author");
-
 CREATE INDEX ON "pages" ("domain");
-
 CREATE INDEX ON "pages" ("url");
 
 CREATE INDEX ON "posts" ("created_at");
-
 CREATE INDEX ON "posts" ("updated_at");
-
 CREATE INDEX ON "posts" ("title");
-
 CREATE INDEX ON "posts" ("created_at", "updated_at");
 
-ALTER TABLE "pages" ADD FOREIGN KEY ("page_author") REFERENCES "users" ("id");
+ALTER TABLE "pages" ADD FOREIGN KEY ("author_id") REFERENCES "users" ("id");
+ALTER TABLE "pages" ADD FOREIGN KEY ("page_author") REFERENCES "users" ("username");
 
-ALTER TABLE "posts" ADD FOREIGN KEY ("post_author") REFERENCES "users" ("id");
+ALTER TABLE "posts" ADD FOREIGN KEY ("author_id") REFERENCES "users" ("id");
+ALTER TABLE "posts" ADD FOREIGN KEY ("post_author") REFERENCES "users" ("username");
+ALTER TABLE "posts" ADD FOREIGN KEY ("published_by") REFERENCES "users" ("username");
+ALTER TABLE "posts" ADD FOREIGN KEY ("updated_by") REFERENCES "users" ("username");
 
 ALTER TABLE "meta" ADD FOREIGN KEY ("page_id") REFERENCES "pages" ("id");
-
 ALTER TABLE "meta" ADD FOREIGN KEY ("posts_id") REFERENCES "posts" ("id");
