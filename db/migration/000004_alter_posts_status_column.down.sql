22e5962e5bb510e382fef631cc0c376b20fe72cb
minierparedes <3thancr0wn@gmail.com>
2024-05-13 12:02:11 +0900

    chore(sql): alter status's type var -> enum

---
+++ db/migration/000004_alter_posts_status_column.down.sql
@@ -0,0 +1,11 @@
+-- Rollback the changes to status column
+DO $$
+BEGIN
+    IF EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name = 'posts') THEN
+        ALTER TABLE posts
+            DROP COLUMN IF EXISTS status;
+        
+        ALTER TABLE posts
+            ADD COLUMN IF NOT EXISTS status varchar(255);
+    END IF;
+END $$;
---
+++ db/migration/000004_alter_posts_status_column.up.sql
@@ -0,0 +1,11 @@
+-- Alter the posts table to change the status column type
+DO $$
+BEGIN
+    IF EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name = 'posts') THEN
+        ALTER TABLE posts
+            DROP COLUMN IF EXISTS status;
+
+        ALTER TABLE posts
+            ADD COLUMN IF NOT EXISTS status access NOT NULL;
+    END IF;
+END $$;
