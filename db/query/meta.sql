a8b7410479107f4465bce50ce5098a1f327bf318
minierparedes <3thancr0wn@gmail.com>
2024-05-24 13:18:31 +0900

    refactor(store): add metas queries for update posts/page

--- db/query/meta.sql
+++ db/query/meta.sql
@@ -19,6 +19,16 @@ INSERT INTO meta (
 SELECT * FROM meta
 WHERE id = $1 LIMIT 1;
 
+-- name: GetMetaByPageIDForUpdate :one
+SELECT * FROM meta
+WHERE page_id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+-- name: GetMetaByPostsIDForUpdate :one
+SELECT * FROM meta
+WHERE posts_id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
 -- name: ListMeta :many
 SELECT * FROM meta
 ORDER BY id
