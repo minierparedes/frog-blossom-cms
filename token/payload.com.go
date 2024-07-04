650651ec30f7f169e8839a982fb057af9abb2a0f
minierparedes <3thancr0wn@gmail.com>
2024-07-04 22:20:40 +0900

    feat(token): add payload jwt func/struct

---
+++ token/payload.com.go
@@ -0,0 +1,30 @@
+package token
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+// Payload data of token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// NewPayload creates new token payload with specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
