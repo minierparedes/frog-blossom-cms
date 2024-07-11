38bbb7bb0e55ec2d2f1c278044fe66c86e854f1e
minierparedes <3thancr0wn@gmail.com>
2024-07-11 15:47:25 +0900

    feat(rules): add authorization rules

--- internal/roles/actions.go
+++ internal/roles/actions.go
@@ -6,12 +6,18 @@ const (
 	CreateUser Action = "CREATE_USER"
 	GetUser    Action = "GET_USER"
 	ListUsers  Action = "LIST_USERS"
+	UpdateUser Action = "UPDATE_USER"
+	DeleteUser Action = "DELETE_USER"
 	CreatePost Action = "CREATE_POST"
-	CreatePage Action = "CREATE_PAGE"
 	GetPost    Action = "GET_POST"
 	ListPosts  Action = "LIST_POSTS"
+	UpdatePost Action = "UPDATE_POST"
+	DeletePost Action = "DELETE_POST"
+	CreatePage Action = "CREATE_PAGE"
 	GetPage    Action = "GET_PAGE"
 	ListPages  Action = "LIST_PAGES"
+	UpdatePage Action = "UPDATE_PAGE"
+	DeletePage Action = "DELETE_PAGE"
 )
 
 var RolePermissions = map[string][]Action{
@@ -19,20 +25,31 @@ var RolePermissions = map[string][]Action{
 		CreateUser,
 		GetUser,
 		ListUsers,
+		UpdateUser,
+		DeleteUser,
 		CreatePost,
-		CreatePage,
 		GetPost,
 		ListPosts,
+		UpdatePost,
+		DeletePost,
+		CreatePage,
 		GetPage,
 		ListPages,
+		UpdatePage,
+		DeletePage,
 	},
 	User: {
 		GetUser,
+		UpdateUser, // only him/herself
 		CreatePost,
-		CreatePage,
 		GetPost,
 		ListPosts,
+		UpdatePost,
+		DeletePost,
+		CreatePage,
 		GetPage,
 		ListPages,
+		UpdatePage,
+		DeletePage,
 	},
 }
